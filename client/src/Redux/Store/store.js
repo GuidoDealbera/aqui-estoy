9d395a0acfe34129270ee82d96d3a4a88c20933b
Pedro Miranda Nowak <pedromirandanowak@gmail.com>
2023-04-25 13:36:05 -0300

    Added some fixes

--- client/src/Redux/Store/store.js
+++ client/src/Redux/Store/store.js
@@ -1,11 +1,13 @@
-import { createStore, applyMiddleware, combineReducers } from "redux";
+import { createStore, applyMiddleware, combineReducers, compose} from "redux";
 import thunk from "redux-thunk";
 import authReducer from "../Reducer/authReducer";
+
+const composeEnhancer = window.REDUX_DEVTOOLS_EXTENSION_COMPOSE || compose
 
 const rootReducer = combineReducers({
   auth: authReducer,
 });
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)));
 
 export default store;
