13309372bd59e1fbcc03cd934ccc6d68d23e92d6
Hernan Rotellini <hernanrotellini@hotmail.com>
2023-04-25 17:46:38 -0300

    postSupervisor + ruta

---
+++ api/src/Routes/routes.js
@@ -0,0 +1,9 @@
+const { Router } = require('express');
+const router = Router();
+const postCompanion = require("../Controllers/postCompanion")
+const postSupervisor = require("../Controllers/postSupervisor")
+
+router.post("/postCompanion", postCompanion);
+router.post("/postSupervisor", postSupervisor);
+
+module.exports = router;
