8d1ba0091e85094a37d432ea77d533301c59b33e
carlavega13 <109261938+carlavega13@users.noreply.github.com>
2023-04-25 13:46:24 -0300

    arreglo de la store con el combine reducer

--- client/src/Redux/Store/store.js
+++ client/src/Redux/Store/store.js
@@ -1,11 +1,28 @@
-import { createStore, applyMiddleware, combineReducers } from "redux";
-import thunk from "redux-thunk";
-import authReducer from "../Reducer/authReducer";
+// import { createStore, applyMiddleware, combineReducers } from "redux";
+// import thunk from "redux-thunk";
+// import authReducer from "../Reducer/authReducer";
 
+// const rootReducer = combineReducers({
+//   auth: authReducer,
+// });
+
+// const store = createStore(rootReducer, applyMiddleware(thunk));
+
+// export default store;
+import { createStore, applyMiddleware, compose, combineReducers } from "redux";
+import authReducer from "../Reducer/authReducer";
+import ThunkMiddleware from "redux-thunk";
+
+//?     DEV TOOLS
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const rootReducer = combineReducers({
   auth: authReducer,
 });
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+//?      STORE
+const store = createStore(
+  rootReducer,
+  composeEnhancer(applyMiddleware(ThunkMiddleware))
+);
 
 export default store;
