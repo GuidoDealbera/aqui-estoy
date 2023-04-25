602ee8783a033bc6bdbf2075389533bcc257849b
LuciaBe <99266309+LuciaBe@users.noreply.github.com>
2023-04-25 14:37:16 -0300

    Update Supervisor.js

--- api/src/Models/Supervisor.js
+++ api/src/Models/Supervisor.js
@@ -2,7 +2,7 @@ const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
   
-  sequelize.define('Dogs', {
+  sequelize.define('Supervisor', {
     id: {
       type: DataTypes.UUID,
       primaryKey: true,
