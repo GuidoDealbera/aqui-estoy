80861674418c7b773d0e06df0fe07d51a0d98e77
carlavega13 <109261938+carlavega13@users.noreply.github.com>
2023-04-25 18:05:15 -0300

    ruta y controlador putCompanion

---
+++ api/src/Controllers/putCompanion.js
@@ -0,0 +1,40 @@
+const Companion = require("../Models/Companion");
+const putCompanion = async (req, res) => {
+  const [
+    name,
+    lastName,
+    profilePhoto,
+    birthdayDate,
+    nationality,
+    country,
+    cityTimeZone,
+    phone,
+    profession,
+    studies,
+    gender,
+  ] = req.body;
+  const { id } = req.params;
+  try {
+    const result = await Companion.update(
+      {
+        name,
+        lastName,
+        profilePhoto,
+        birthdayDate,
+        nationality,
+        country,
+        cityTimeZone,
+        phone,
+        profession,
+        studies,
+        gender,
+      },
+      { where: { id: id } }
+    );
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(400).json(error.message);
+  }
+};
+
+module.exports = putCompanion;
--- api/src/Routes/routes.js
+++ api/src/Routes/routes.js
@@ -1,10 +1,11 @@
-const { Router } = require('express');
+const { Router } = require("express");
 const router = Router();
-const postCompanion = require("../Controllers/postCompanion")
-const postSupervisor = require("../Controllers/postSupervisor")
+const postCompanion = require("../Controllers/postCompanion");
+const postSupervisor = require("../Controllers/postSupervisor");
+const putCompanion = require("../Controllers/putCompanion");
 
 router.post("/postCompanion", postCompanion);
 router.post("/postSupervisor", postSupervisor);
-
+router.put("/putCompanion", putCompanion);
 
 module.exports = router;
