d3d81129ea12835b87e19d212e2da59e6e7aea64
Hernan Rotellini <hernanrotellini@hotmail.com>
2023-04-25 19:11:27 -0300

    Server corriendo y putSuperAdmin

--- api/src/Routes/routes.js
+++ api/src/Routes/routes.js
@@ -4,9 +4,13 @@ const postCompanion = require("../Controllers/postCompanion");
 const postSupervisor = require("../Controllers/postSupervisor");
 const putCompanion = require("../Controllers/putCompanion");
 const putSupervisor = require("../Controllers/putSupervisor");
-
+const {requireSuperAdmin,putSuperAdmin}= require("../Controllers/putSuperAdmin")
 router.post("/postCompanion", postCompanion);
 router.post("/postSupervisor", postSupervisor);
 router.put("/putCompanion", putCompanion);
 router.put("/putSupervisor", putSupervisor);
+
+
+router.put("/isSuperAdmin",)
+
 module.exports = router;
--- api/src/db.js
+++ api/src/db.js
@@ -7,7 +7,7 @@ const {
     DB_USER, DB_PASSWORD, DB_HOST,
   } = process.env;
 
-  const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/aquiEstoyDb`, {
+  const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/aquiestoydb`, {
   logging: false, 
   native: false, 
 });
