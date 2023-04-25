ebae2bb6b60ec8302a7578f613259f4c10e77bb0
GuidoDealbera <guidojdealbera@gmail.com>
2023-04-25 15:40:30 -0300

    Componente NavBar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,13 +4,17 @@ import React from 'react';
 import {Routes, Route} from 'react-router-dom';
 import Profile from './Components/Profile/Profile';
 import Landing from './Views/LandingPage/Landing';
+import NavBar from './Components/NavBar/NavBar';
 
 function App() {
   return (
+    <div>
+      <NavBar/> 
     <Routes>
       <Route exact path='/' element={<Landing/>}/>
       <Route path='/profile/:name' element={<Profile/>}/>
     </Routes>
+    </div>
   );
 }
 
