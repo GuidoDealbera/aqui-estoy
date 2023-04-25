02f0c514756b932287a57d33eca3a08b43f6bfa9
carlavega13 <109261938+carlavega13@users.noreply.github.com>
2023-04-25 18:19:24 -0300

    controlador y ruta putSupervisor

--- api/src/Routes/routes.js
+++ api/src/Routes/routes.js
@@ -3,9 +3,10 @@ const router = Router();
 const postCompanion = require("../Controllers/postCompanion");
 const postSupervisor = require("../Controllers/postSupervisor");
 const putCompanion = require("../Controllers/putCompanion");
+const putSupervisor = require("../Controllers/putSupervisor");
 
 router.post("/postCompanion", postCompanion);
 router.post("/postSupervisor", postSupervisor);
 router.put("/putCompanion", putCompanion);
-
+router.put("/putSupervisor", putSupervisor);
 module.exports = router;
