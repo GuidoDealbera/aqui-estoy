f227f7b5520955b9525fd478f938298b73ae4c36
juanferjaramillo <juanfer.jaramillo@gmail.com>
2023-04-26 10:56:17 -0500

    modelos shift

--- api/src/Models/SupervisorShift.js
+++ api/src/Models/SupervisorShift.js
@@ -2,7 +2,7 @@ const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
   sequelize.define(
-    "VolunteerShift",
+    "SupervisorShift",
     {
       id: {
         type: DataTypes.UUID,
--- api/src/db.js
+++ api/src/db.js
@@ -1,16 +1,17 @@
-const { Sequelize } = require('sequelize');
-require('dotenv').config();
-const fs = require('fs');
-const path = require('path');
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
+const fs = require("fs");
+const path = require("path");
 
-const {
-    DB_USER, DB_PASSWORD, DB_HOST,
-  } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
-  const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/aquiestoydb`, {
-  logging: false, 
-  native: false, 
-});
+const sequelize = new Sequelize(
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/aquiestoydb`,
+  {
+    logging: false,
+    native: false,
+  }
+);
 
 const basename = path.basename(__filename);
 
@@ -19,29 +20,39 @@ console.log(basename);
 const modelDefiners = [];
 
 // Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
-fs.readdirSync(path.join(__dirname, '/models'))
-  .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
+fs.readdirSync(path.join(__dirname, "/models"))
+  .filter(
+    (file) =>
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+  )
   .forEach((file) => {
-    modelDefiners.push(require(path.join(__dirname, '/models', file)));
+    modelDefiners.push(require(path.join(__dirname, "/models", file)));
   });
 
-  // Injectamos la conexion (sequelize) a todos los modelos
-modelDefiners.forEach(model => model(sequelize));
+// Injectamos la conexion (sequelize) a todos los modelos
+modelDefiners.forEach((model) => model(sequelize));
 // Capitalizamos los nombres de los modelos ie: product => Product
 let entries = Object.entries(sequelize.models);
-let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
+let capsEntries = entries.map((entry) => [
+  entry[0][0].toUpperCase() + entry[0].slice(1),
+  entry[1],
+]);
 sequelize.models = Object.fromEntries(capsEntries);
 
 //*MODELS:
-const { Companion, Supervisor } = sequelize.models; 
+const { Companion, Supervisor, SupervisorShift, VolunteerShift } =
+  sequelize.models;
 
-//* RELATIONS N-1: 
+//* RELATIONS N-1:
 Supervisor.hasMany(Companion);
 Companion.belongsTo(Supervisor);
+
+Companion.belongsToMany(VolunteerShift, { through: "CompaShift" });
+Supervisor.belongsToMany(SupervisorShift, { through: "SupervShift" });
 
 //* RELACIONES N-N TURNOS:
 
 module.exports = {
-    ...sequelize.models, 
-    conn: sequelize,     
-  };
+  ...sequelize.models,
+  conn: sequelize,
+};
