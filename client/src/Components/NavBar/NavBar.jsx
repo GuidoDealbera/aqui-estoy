a0321554cfa605bb6f2a509a329767ec357c4c24
juanferjaramillo <juanfer.jaramillo@gmail.com>
2023-04-26 13:07:49 -0500

    navbar useless

--- client/src/Components/NavBar/NavBar.jsx
+++ client/src/Components/NavBar/NavBar.jsx
@@ -2,19 +2,20 @@ import image from "../../img/aquiestoy.jpg";
 import { useNavigate, useLocation } from "react-router-dom";
 import LoginForm from "../LoginForm/LoginForm";
 import { useState } from "react";
-import styles from './NavBar.module.css'
-
+import styles from "./NavBar.module.css";
 
 export default function NavBar(props) {
   const location = useLocation();
   const navigate = useNavigate();
   const [showLogin, setShowLogin] = useState(false);
+  
   const handleMouseEnter = () => {
-    setShowLogin(true)
+    setShowLogin(true);
   };
   const handleMouseLeave = () => {
-    setShowLogin(false)
-  }
+    setShowLogin(false);
+  };
+
   const handleClick = (event) => {
     //const { name } = event.target;
     // switch (name) {
@@ -41,13 +42,21 @@ export default function NavBar(props) {
         Calendario
       </button>
       <img src={image} alt="aquiEstoy" />
-      {location.pathname !== '/profile/Chiringuito' && <button name='session' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-        Iniciar sesión
-      </button>}
-      {showLogin && <LoginForm setShowLogin={setShowLogin}/>}
-      {location.pathname !== '/profile/Chiringuito' && <button name="register" onClick={handleClick}>
-        Registrate
-      </button>}
+      {location.pathname !== "/profile/Chiringuito" && (
+        <button
+          name="session"
+          onMouseEnter={handleMouseEnter}
+          //onMouseLeave={handleMouseLeave} 
+        >
+          Iniciar sesión
+        </button>
+      )}
+      {showLogin && <LoginForm setShowLogin={setShowLogin} />}
+      {location.pathname !== "/profile/Chiringuito" && (
+        <button name="register" onClick={handleClick}>
+          Registrate
+        </button>
+      )}
     </div>
   );
 }
