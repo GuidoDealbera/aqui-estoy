32671829b3c43623a3db3692b106159335a4982f
GuidoDealbera <guidojdealbera@gmail.com>
2023-04-26 12:56:02 -0300

    push provisorio

---
+++ client/src/Views/Profiles/Profiles.jsx
@@ -0,0 +1,10 @@
+import Companion from "../../Components/Perfiles/Companion/Companion"
+
+export default function Profile (props){
+   
+    return (
+        <div>
+           <Companion/>
+        </div>
+    )
+}
--- client/src/data.js
+++ client/src/data.js
@@ -1,23 +1,46 @@
-const prueba = [
-    {
-        profileImage: 'https://www.tuexperto.com/wp-content/uploads/2015/07/perfil_01.jpg',
-        name: 'Chiringuito',
-        lastName: 'Vega',
-        birthDate: '2022/04/25',
-        nacionality: 'Argentino',
-        country: 'Argentina',
-        timeZone: 'GTM -3',
-        email: 'chirivega@test.com',
-        password: 'chirivega123',
-        phone: '94-2834527',
-        profession: 'Arquitecto',
-        studies: 'Titulo universitario',
-        gender: 'Masculino',
-    },
-    {
-        email: 'fulanito.test@mail.com',
-        password: 'test1234'
-    }
+export const prueba = [
+  {
+    profileImage:
+      "https://www.tuexperto.com/wp-content/uploads/2015/07/perfil_01.jpg",
+    name: "Chiringuito",
+    lastName: "Vega",
+    birthDate: "2022/04/25",
+    nacionality: "Argentino",
+    country: "Argentina",
+    timeZone: "GTM -3",
+    email: "chirivega@test.com",
+    password: "chirivega123",
+    phone: "94-2834527",
+    profession: "Arquitecto",
+    studies: "Titulo universitario",
+    gender: "Masculino",
+    rol: 'SuperAdmin'
+  },
+  {
+    email: "fulanito.test@mail.com",
+    password: "test1234",
+  },
 ];
 
-export default prueba;
+export const voluntario = [
+  {
+    profileImage: "https://www.tuexperto.com/wp-content/uploads/2015/07/perfil_01.jpg",
+    name: "Charanguito",
+    lastName: "Grillia",
+    birthDate: "2022/04/26",
+    nacionality: "Uruguayo",
+    country: "Argentina",
+    timeZone: "GTM -3",
+    email: "charagrillia@test.com",
+    password: "charagrillia123",
+    phone: "72-68552123",
+    profession: "Programador",
+    studies: "Henry Bootcamp",
+    gender: "Masculino",
+    rol: 'Companion'
+  },
+  {
+    email: 'prueba.test@mail.com',
+    password: 'prueba123'
+  }
+];
