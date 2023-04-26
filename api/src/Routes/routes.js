c564e5aab3e6f0e00e01f555e171aba647a3cbd7
LuciaBe <lubellardi@gmail.com>
2023-04-26 12:20:56 -0300

    rutaGetOneSupervisor/lu

--- api/src/Routes/routes.js
+++ api/src/Routes/routes.js
@@ -11,12 +11,15 @@ const {
   putSuperAdmin,
 } = require("../Controllers/putSuperAdmin");
 
+const getOneSupervisor = require("../Controllers/getOneSupervisor")
+
 router.get("/getSupervisor", getSupervisor);
 router.get("/getCompanion", getCompanion);
 router.post("/postCompanion", postCompanion);
 router.post("/postSupervisor", postSupervisor);
 router.put("/putCompanion/:id", putCompanion);
+
 router.put("/putSupervisor/:id", putSupervisor);
 router.put("/isSuperAdmin/:id", requireSuperAdmin, putSuperAdmin);
-
+router.get("/getOneSupervisor", getOneSupervisor);
 module.exports = router;
