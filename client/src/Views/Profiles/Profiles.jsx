b6557e3f986047171e5e18f5ae9883714e323511
Luis <lcbtrejo1206@gmail.com>
2023-04-27 10:29:12 -0400

    supervisor

--- client/src/Views/Profiles/Profiles.jsx
+++ client/src/Views/Profiles/Profiles.jsx
@@ -1,10 +1,11 @@
 import Companion from "../../Components/Perfiles/Companion/Companion"
-
+import Supervisor from "../../Components/Perfiles/Supervisor/Supervisor"
 export default function Profile (props){
    
     return (
         <div>
-           <Companion/>
+           {/* <Companion/> */}
+           <Supervisor/>
         </div>
     )
 }
