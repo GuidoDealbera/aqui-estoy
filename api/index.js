568e0c013b633d8111e93122a84d4ebc2b406244
juanferjaramillo <juanfer.jaramillo@gmail.com>
2023-04-27 21:42:54 -0500

    landing loads redux

--- api/index.js
+++ api/index.js
@@ -7,8 +7,8 @@ const {
   fillCompanionShifts,
 } = require("./src/Controllers/getCompanionShift.js");
 const postCityTimeZone = require("./src/Controllers/postCityTimeZone.js");
-conn.sync({ alter: true }).then(async () => {
-// conn.sync({ force: true }).then(async () => {
+// conn.sync({ alter: true }).then(async () => {
+conn.sync({ force: true }).then(async () => {
   await fillCompanionShifts();
   await fillSupervisorShifts();
   await postCityTimeZone();
