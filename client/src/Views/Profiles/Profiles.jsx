40e06c560c3b7a12ef8d9ac7e8638361e2832777
GuidoDealbera <guidojdealbera@gmail.com>
2023-04-28 13:30:48 -0300

    actions y imports views

--- client/src/Views/Profiles/Profiles.jsx
+++ client/src/Views/Profiles/Profiles.jsx
@@ -1,7 +1,8 @@
 import Companion from "../../Components/Perfiles/Companion/Companion"
 import Supervisor from "../../Components/Perfiles/Supervisor/Supervisor"
+import SuperAdmin from "../../Components/Perfiles/SuperAdmin/SuperAdmin"
 
-export default function Profile (props){
+export default function Profiles (props){
    
     return (
         <div>
