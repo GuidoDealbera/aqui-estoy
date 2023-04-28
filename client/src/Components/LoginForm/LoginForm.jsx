24d01bc76bb866b68b3a3a7c3cd20ecdd9924f1a
Pedro Miranda Nowak <pedromirandanowak@gmail.com>
2023-04-28 13:22:45 -0300

    .

--- client/src/Components/LoginForm/LoginForm.jsx
+++ client/src/Components/LoginForm/LoginForm.jsx
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import axios from "axios"
+import { useDispatch, useSelector } from "react-redux"
+import { getBothRoles } from '../../Redux/Actions/viewActions';
+import { useNavigate } from "react-router-dom"
 import {
   TextField,
   Button,
@@ -16,16 +19,63 @@ const validationSchema = Yup.object().shape({
   password: Yup.string().min(6, 'Must be at least 6 characters').required('Required'),
 });
 
-const LoginForm = ({ onSubmit, handleMouseLeave }) => {
-  const [rol,setRol]=useState("Acompañante")
-  const toggle=(event)=>{
-  if(event.target.value==="Acompañante"){
-    setRol("Supervisor")
-  }else{
-    setRol("Acompañante")
+const LoginForm = ({ handleMouseLeave }) => {
+
+  const [rol, setRol] = useState("Acompañante")
+  const toggle = (event) => {
+    if (event.target.value === "Acompañante") {
+      setRol("Supervisor")
+    } else {
+      setRol("Acompañante")
+    }
   }
+
+  const navigate = useNavigate()
+
+  const dispatch = useDispatch();
+
+  const submitHandler = async (values) => {
+
+    // const { email, password } = values;
+    // if (rol === "Supervisor") {
+    //   dispatch(getBothRoles(email, password))
+    //   try {
+    //     const response = await axios.post("http://localhost:3001/getBothRoles", {
+    //       email, password
+    //     })
+    //     const { data } = response;
+    //     const { name } = data;
+    //     console.log(name);
+    //   } catch (error) {
+
+    //   }
+    // } else {
+    //   dispatch(getBothRoles(email, password))
+    //   const response = await axios.post("http://localhost:3001/getBothRoles", {
+    //     email, password
+    //   })
+    //   const { data } = response;
+    //   const { name } = data;
+    //   console.log(name);
+    // }
+
+    // // if (typeof user !== "string") {
+    // //   const { name } = user;
+    // //   if (name) {
+    // //     navigate("/profile/Chiringuito")
+    // //   } else {
+    // //     navigate("/register")
+    // //   }
+    // //   handleMouseLeave()
+    // // }
+
   }
-    return (
+
+  const user = useSelector((state) => {
+    return state.auth.user
+  })
+
+  return (
     <Formik
       initialValues={{ email: '', password: '' }}
       validationSchema={validationSchema}
@@ -37,7 +87,7 @@ const LoginForm = ({ onSubmit, handleMouseLeave }) => {
             <Grid container spacing={2}>
               <Grid item xs={12}>
                 <Typography variant="h5">Iniciar sesión</Typography>
-          <button value={rol} type='button' onClick={toggle} name="Acompañante">{rol}</button>
+                <button value={rol} type='button' onClick={toggle} name="Acompañante">{rol}</button>
               </Grid>
               <Grid item xs={12}>
                 <Field
@@ -69,7 +119,7 @@ const LoginForm = ({ onSubmit, handleMouseLeave }) => {
                     variant="contained"
                     color="primary"
                     type="submit"
-                    disabled={isSubmitting}
+                  // disabled={isSubmitting}
                   >
                     Iniciar Sesión
                   </Button>
