ef92eeb0324975e852c7320ea59666889f262255
GuidoDealbera <guidojdealbera@gmail.com>
2023-04-29 20:13:53 -0300

    Fixed Register

--- client/src/Views/Register/Register.jsx
+++ client/src/Views/Register/Register.jsx
@@ -1,32 +1,20 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import axios from "axios"
-import allCountries from './data';
 import { Select, MenuItem, InputLabel, Button, Typography, TextField, Box, Input } from '@mui/material';
 import { putCompanion, putSupervisor } from '../../Redux/Actions/postPutActions';
 import {useDispatch, useSelector} from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import CountrySelect from './registerComponents/CountrySelect';
+import TimezoneSelect from "./registerComponents/TimeZoneSelect";
+
 
 
 export default function Register() {
-
-    const [timezones, setTimezones] = useState([]);
     const dispatch = useDispatch();
     const {user} = useSelector(state=> state.auth)
     const navigate = useNavigate();   
-  
-    useEffect(() => {
-        axios("http://localhost:3001/getCityTimeZone").then(
-            (response) => {
-                const { data } = response;
-                const result = data.map((timezone) => {
-                    return `${timezone.offSet} ${timezone.zoneName}`
-                })
-                setTimezones(result)
-            }
-        )
-    }, [])
 
     const [index, setIndex] = useState(0) //Estado del "paginado" de los inputs (Mirar Figma).
 
@@ -48,7 +36,7 @@ export default function Register() {
     }
 
     const submitHandler = (values) => { //Submit Handler del formulario (Aún no interactúa con el Back-End)
-  
+        console.log(values);
      if(user.rol === 'Companion' || user.rol === "Acompañante2"){
         dispatch(putCompanion(user.id, values)) //trae el id del user y lo actualiza 
      } else{
@@ -60,26 +48,23 @@ export default function Register() {
 
     const validationSchema = Yup.object().shape({ //Validaciones de Yup (Aún en desarrollo)
         profilePhoto: Yup.string()
-            .url('URL de la imágen inválida'),
-        name: Yup.string()
-            .max(15, "Debe ser menor a 15 caracteres")
-            .required('Este campo es obligatorio'),
-        lastName: Yup.string()
-            .max(20, "Debe ser menor a 20 caracteres")
-            .required('Este campo es obligatorio'),
-        birthdayDate: Yup.date()
-            .required('Este campo es obligatorio'),
-        nationality: Yup.string(),
-        country: Yup.string(),
-        cityTimeZone: Yup.string(),
-        email: Yup.string()
-            .email('El e-mail proporcionado no es válido')
-            .required('Este campo es obligatorio'),
-        phone: Yup.string()
-            .matches(/^\+?[0-9\s]*[1-9][0-9]*$/, 'El número de teléfono debe contener solo números y espacios en blanco')
-            .test('is-positive', 'El número de teléfono debe ser positivo', (value) => !value || parseInt(value.replace(/\s+/g, '')) > 0),
-        studies: Yup.string(),
-        gender: Yup.string(),
+        .url('URL de la imágen inválida'),
+    name: Yup.string()
+        .max(15, "Debe ser menor a 15 caracteres")
+        .required('Este campo es obligatorio'),
+    lastName: Yup.string()
+        .max(20, "Debe ser menor a 20 caracteres")
+        .required('Este campo es obligatorio'),
+    birthdayDate: Yup.date()
+        .required('Este campo es obligatorio'),
+    nationality: Yup.string(),
+    country: Yup.string(),
+    cityTimeZone: Yup.string(),
+    phone: Yup.string()
+        .matches(/^\+?[0-9\s]*[1-9][0-9]*$/, 'El número de teléfono debe contener solo números y espacios en blanco')
+        .test('is-positive', 'El número de teléfono debe ser positivo', (value) => !value || parseInt(value.replace(/\s+/g, '')) > 0),
+    studies: Yup.string(),
+    gender: Yup.string(),
     });
 
     //Estas 3 páginas es simplemente código html dividido en 3 partes, para hacer el paginado de "Siguiente", "Anterior" (Mirar Figma)
@@ -87,7 +72,7 @@ export default function Register() {
     const firstPage = <>
         <Box>
             <InputLabel>Foto de Perfil</InputLabel>
-            <Field name="profilePhoto">
+            <Field sx={{ width: 300 }} name="profilePhoto">
                 {({ field, form }) => (
                     <>
                         <Input
@@ -107,27 +92,27 @@ export default function Register() {
                     </>
                 )}
             </Field>
-            <ErrorMessage name='profilePhoto'>
+            <ErrorMessage name='profilePhoto' sx={{ width: 300 }}>
                 {msg => <Typography color="error">{msg}</Typography>}
             </ErrorMessage>
         </Box>
         <Box>
             <InputLabel>Nombre</InputLabel>
-            <Field as={TextField} name="name" />
+            <Field as={TextField} name="name" sx={{ width: 300 }} />
             <ErrorMessage name='name'>
                 {msg => <Typography color="error">{msg}</Typography>}
             </ErrorMessage>
         </Box>
         <Box>
             <InputLabel>Apellido</InputLabel>
-            <Field as={TextField} name="lastName" />
+            <Field as={TextField} name="lastName" sx={{ width: 300 }}/>
             <ErrorMessage name='lastName'>
                 {msg => <Typography color="error">{msg}</Typography>}
             </ErrorMessage>
         </Box>
         <Box>
             <InputLabel>Fecha de Nacimiento</InputLabel>
-            <Field as={TextField} name="birthdayDate" type="date" />
+            <Field as={TextField} name="birthdayDate" type="date" sx={{ width: 300 }}/>
             <ErrorMessage name='birthdayDate'>
                 {msg => <Typography color="error">{msg}</Typography>}
             </ErrorMessage>
@@ -139,15 +124,9 @@ export default function Register() {
         <Box>
             <InputLabel>Nacionalidad</InputLabel>
             <Field
-                as={Select}
-                name="country"
+                component={CountrySelect}
+                name="nationality"
             >
-                <MenuItem value="" disabled>Select an option</MenuItem>
-                {allCountries.map((country, i) => (
-                    <MenuItem key={i} value={country}>
-                        {country}
-                    </MenuItem>
-                ))}
             </Field>
             <ErrorMessage name='nationality'>
                 {msg => <Typography color="error">{msg}</Typography>}
@@ -155,11 +134,7 @@ export default function Register() {
         </Box>
         <Box>
             <InputLabel>Pais de residencia actual</InputLabel>
-            <Field as={Select} name="residence">
-                <MenuItem value="" disabled>Select an option</MenuItem>
-                {allCountries.map((country, i) => {
-                    return <MenuItem key={i} value={country}>{country}</MenuItem>
-                })}
+            <Field component={CountrySelect} name="residence">
             </Field>
             <ErrorMessage name='country'>
                 {msg => <Typography color="error">{msg}</Typography>}
@@ -167,38 +142,46 @@ export default function Register() {
         </Box>
         <Box>
             <InputLabel>Ciudad / huso horario de residencia</InputLabel>
-            <Field as={Select} name="cityTimeZone">
-                <MenuItem value="" disabled>Select an option</MenuItem>
-                {timezones.map((timezone, i) => {
-                    return <MenuItem value={timezone} key={i}>{timezone}</MenuItem>
-                })}
+            <Field
+                component={TimezoneSelect}
+                name="cityTimeZone"
+            >
             </Field>
             <ErrorMessage name='cityTimeZone'>
                 {msg => <Typography color="error">{msg}</Typography>}
             </ErrorMessage>
         </Box>
-        <Box>
-            <InputLabel>E-mail</InputLabel>
-            <Field as={TextField} name='email' />
+        {/* <Box>
+            <Field
+                as={TextField}
+                sx={{ width: 300, marginTop: "23px" }}
+                id="email"
+                label="Email"
+                name="email"
+                type="name"
+                error={!!(ErrorMessage.name)}
+                helperText={<ErrorMessage name="Email" />}
+            />
             <ErrorMessage name='email'>
                 {msg => <Typography color="error">{msg}</Typography>}
             </ErrorMessage>
-        </Box>
+        </Box> */}
         <Button name="Anterior" onClick={clickHandler}>Volver</Button>
         <Button name="Siguiente" onClick={clickHandler}>Continuar</Button>
+
     </>
 
     const thirdPage = <>
         <Box>
             <InputLabel>Teléfono</InputLabel>
-            <Field name='phone' as={TextField} />
+            <Field name='phone' as={TextField} sx={{ width: 300 }} />
             <ErrorMessage name='phone'>
                 {msg => <Typography color="error">{msg}</Typography>}
             </ErrorMessage>
         </Box>
         <Box>
             <InputLabel>¿Estudias o trabajas en alguna de estas areas?</InputLabel>
-            <Field as={Select} name="profession">
+            <Field as={Select} sx={{ width: 300 }} name="profession">
                 <MenuItem value="">Select an option</MenuItem>
                 <MenuItem value="Yes">Sí</MenuItem>
                 <MenuItem value="No">No</MenuItem>
@@ -209,7 +192,7 @@ export default function Register() {
         </Box>
         <Box>
             <InputLabel>Estudios alcanzados</InputLabel>
-            <Field as={Select} name="studies">
+            <Field as={Select} sx={{ width: 300 }} name="studies">
                 <MenuItem value="">Select an option</MenuItem>
                 <MenuItem value="Primario">Educación Inicial</MenuItem>
                 <MenuItem value="Secundario">Educación Primaria</MenuItem>
@@ -222,7 +205,7 @@ export default function Register() {
         </Box>
         <Box>
             <InputLabel>¿Con qué genero te identificas?</InputLabel>
-            <Field as={Select} name="gender">
+            <Field as={Select} sx={{ width: 300 }} name="gender">
                 <MenuItem value="">Select an option</MenuItem>
                 <MenuItem value="Femenino">Femenino</MenuItem>
                 <MenuItem value="Masculino">Masculino</MenuItem>
@@ -247,22 +230,22 @@ export default function Register() {
                 nationality: "",
                 country: "",
                 cityTimeZone: "",
-                email: "",
                 phone: "",
                 profession: "",
                 studies: "",
-                gender: ""
+                gender: "",
+                rol: user.rol
             }}
     validationSchema={validationSchema}
             onSubmit={submitHandler}
         >
-            {(formik) => {
+            {(props) => {
                 return (
                     <Form>
                         {index === 0 ? firstPage : null}
                         {index === 1 ? secondPage : null}
                         {index === 2 ? thirdPage : null}
-                        {formik.errors && Object.keys(formik.errors).length > 0 &&
+                        {props.errors && Object.keys(props.errors).length > 0 &&
                             <Typography>Hay errores en los campos. Por favor, revíselos.</Typography>
                         }
                     </Form>
