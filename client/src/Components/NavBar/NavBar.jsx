af4967a4368638f7dcad2e4eee61d4456a693cb7
GuidoDealbera <guidojdealbera@gmail.com>
2023-04-30 16:23:59 -0300

    Fixed Navbar & LogOut

--- client/src/Components/NavBar/NavBar.jsx
+++ client/src/Components/NavBar/NavBar.jsx
@@ -1,4 +1,4 @@
-import image from "../../img/aquiestoy.jpg";
+import image from "../../img/aquiestoy(bien).jpg";
 import { Link, useLocation } from "react-router-dom";
 import LoginForm from "../LoginForm/LoginForm";
 import { useState } from "react";
@@ -13,13 +13,15 @@ import {
   IconButton,
 } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { logOut } from "../../Redux/Actions/viewActions";
 
 export default function NavBar(props) {
   const {user} = useSelector((state) => state.auth);
   const {id} = user;
   const location = useLocation();
+  const dispatch = useDispatch();
   const [showLogin, setShowLogin] = useState(false);
   const [anchorEl, setAnchorEl] = useState(null);
   const navigate = useNavigate();
@@ -37,6 +39,10 @@ export default function NavBar(props) {
     navigate("/calendary")
   }
   };
+  const closeSession = () => {
+    dispatch(logOut());
+    navigate('/');
+  }
 
   const handleMenuClick = (event) => {
     setAnchorEl(event.currentTarget);
@@ -54,16 +60,16 @@ export default function NavBar(props) {
   return (
     <Box
       sx={{
-        background: "#F6F6F6",
+        background: "#C8CCD8",
       }}
     >
       <Grid container justifyContent="space-between" alignItems="center">
         <Grid item>
           <Box>
             <Hidden smDown>
-              <Button variant="text" name="about" onClick={handleClick}>
+              {/* <Button variant="text" name="about" onClick={handleClick}>
                 Acerca de
-              </Button>
+              </Button> */}
               <Button variant="text" name="calendar" onClick={handleClick}>
                 Calendario
               </Button>
@@ -97,12 +103,14 @@ export default function NavBar(props) {
         </Grid>
         <Grid item>
           <Link to="/">
+            <Box  sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
           <CardMedia
             component="img"
             style={{ height: "20%", width: "20%" }}
             image={image}
             alt="aquiEstoy"
           />
+            </Box>
           </Link>
         </Grid>
         <Grid item>
@@ -112,6 +120,9 @@ export default function NavBar(props) {
               {Object.entries(user).length === 0?"Iniciar sesión":"Perfil"}
               </Button>
             )}
+            {location.pathname !== '/' && <Button variant="text" name="logout" onClick={closeSession}>
+                Cerrar Sesión
+              </Button>}
             {showLogin && <LoginForm handleMouseLeave={handleMouseLeave} />}
           </Box>
         </Grid>
