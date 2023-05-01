1284a570d996c4730d9e0a81051de8d6fc36d509
Hernan Rotellini <91641938+HernanRotellini@users.noreply.github.com>
2023-05-01 14:15:10 -0300

    Update getSupervisor.js

--- api/src/Controllers/Supervisor/getSupervisor.js
+++ api/src/Controllers/Supervisor/getSupervisor.js
@@ -6,7 +6,7 @@ const getSupervisor = async (req,res)=>{
         //Buscar todos los Supervisores guardados en bd
         const results = await Supervisor.findAll();
         //Retorna todos los supervisores como un array de objetos
-        res.json(results)
+        res.status(200).json(results)
     }
     catch(error){
         res.status(400).json({ error: 'Error al buscar datos' });
