fc1a0fd871a6d9ceb61f023ee597f411f6cad5a4
LuciaBe <lubellardi@gmail.com>
2023-05-01 15:46:35 -0300

    Coreccion shift para que se seteen una sola vez

--- api/index.js
+++ api/index.js
@@ -1,11 +1,7 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
-const {
-  fillSupervisorShifts,
-} = require("./src/Controllers/Supervisor/getSupervisorShift.js");
-const {
-  fillCompanionShifts,
-} = require("./src/Controllers/Companion/getCompanionShift.js");
+const {fillSupervisorShifts} = require("./src/Controllers/Supervisor/getSupervisorShift.js");
+const {fillCompanionShifts} = require("./src/Controllers/Companion/getCompanionShift.js");
 const postCityTimeZone = require("./src/Controllers/TimeZone/postCityTimeZone.js");
 // conn.sync({ alter: true }).then(async () => {
 conn.sync({ force: false }).then(async () => {
