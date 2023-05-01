7242cdd209a480c9577a14ebdb7c7aafd13b724f
GuidoDealbera <guidojdealbera@gmail.com>
2023-05-01 17:12:57 -0300

    viewActions Alerts

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,16 +5,18 @@ import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import store from "./Redux/Store/store.js";
+import { Toaster } from "sonner";
 
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
+        <Toaster position="bottom-right" duration={3000} />
         <App />
       </BrowserRouter>
     </Provider>
