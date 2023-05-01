9861d1c305782294c066ac51e471db73e426c848
GuidoDealbera <guidojdealbera@gmail.com>
2023-05-01 19:55:39 -0300

    Deploy Changes

--- api/src/db.js
+++ api/src/db.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const fs = require("fs");
 const path = require("path");
 
-const { DB_USER, DB_PASSWORD, DB_HOST , DE_DEPLOY} = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST , DB_DEPLOY} = process.env;
 
 const sequelize = new Sequelize(
   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/aquiestoydb`,
