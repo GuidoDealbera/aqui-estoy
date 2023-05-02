78f0a11bfa96c4dc6c1750057023c3724b19a1a1
carlavega13 <109261938+carlavega13@users.noreply.github.com>
2023-05-02 12:39:26 -0300

    Calendario de supervisor funcional

--- api/index.js
+++ api/index.js
@@ -2,24 +2,31 @@ require("dotenv").config();
 const port = process.env.PORT || 3001;
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
-const {fillSupervisorShifts} = require("./src/Controllers/Supervisor/getSupervisorShift.js");
-const {fillCompanionShifts} = require("./src/Controllers/Companion/getCompanionShift.js");
+const {
+  fillSupervisorShifts,
+} = require("./src/Controllers/Supervisor/getSupervisorShift.js");
+const {
+  fillCompanionShifts,
+} = require("./src/Controllers/Companion/getCompanionShift.js");
 const postCityTimeZone = require("./src/Controllers/TimeZone/postCityTimeZone.js");
-const postSupervisor = require("./src/Controllers/Supervisor/postSupervisor.js")
+const postSupervisor = require("./src/Controllers/Supervisor/postSupervisor.js");
 // conn.sync({ alter: true }).then(async () => {
 
 conn.sync({ force: true }).then(async () => {
-  await postSupervisor({}, {
-    status: function(code) {
-      console.log('Status:', code);
-    },
-    json: function(response) {
-      console.log('Response:', response);
-    },
-    send: function(error) {
-      console.log('Error:', error);
+  await postSupervisor(
+    {},
+    {
+      status: function (code) {
+        console.log("Status:", code);
+      },
+      json: function (response) {
+        console.log("Response:", response);
+      },
+      send: function (error) {
+        console.log("Error:", error);
+      },
     }
-  });
+  );
   console.log("Codigo 500 pero creó el administrador");
   await fillCompanionShifts();
   await fillSupervisorShifts();
---
+++ client/src/Components/Calendary/CalendarSupervisorPopOut.jsx
@@ -0,0 +1,24 @@
+import { useDispatch, useSelector } from "react-redux"
+import s from "./CalendarSupervisorPopOut.module.css"
+import { postAssignSupervisorShift } from "../../Redux/Actions/postPutActions"
+
+const CalendarPopOut=(props)=>{
+    const user=useSelector(state=>state.auth.user)
+
+    const dispatch=useDispatch()
+    const handleConfirm=()=>{
+dispatch(postAssignSupervisorShift(user.id,props.shift.id.toString()))
+props.setTrigger()
+alert("Tu turno a sido confirmado")
+    }
+    return (props.trigger)?(
+        <div onClick={()=>props.setTrigger()} className={s.popOut}>
+            <div className={s.innerPop}>
+         {props.children}
+         <button onClick={handleConfirm}>Confirma tu turno !</button>
+         <button onClick={()=>props.setTrigger()}>Cancelar</button>
+            </div>
+        </div>
+    ):""
+}
+export default CalendarPopOut
