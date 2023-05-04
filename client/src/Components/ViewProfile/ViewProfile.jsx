720ef35d74bd7b2791aa8af7d883774a75ef6eed
Pedro Miranda Nowak <pedromirandanowak@gmail.com>
2023-05-04 13:09:45 -0300

    Agregados cambios finales a la Card / Linkeado con el panel de supervision

--- client/src/Components/ViewProfile/ViewProfile.jsx
+++ client/src/Components/ViewProfile/ViewProfile.jsx
@@ -1,52 +1,19 @@
+// IMPORTACIONES DE REACT
 import React from 'react'
 import { useEffect } from 'react';
 import { useParams } from 'react-router-dom'
+// IMPORTACIONES PARA TRAER EL USUARIO
 import { useDispatch, useSelector } from 'react-redux'
 import { getUserById } from '../../Redux/Actions/viewActions';
-import { Card, CardHeader, CardContent, Avatar, Container, Typography, Box } from '@mui/material';
-import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
+// IMPORTACIONES DE MUI/ESTILOS
+import { Avatar, Typography, Box, Grid } from '@mui/material';
+import {useTheme} from '@mui/material';
+import styles from './ViewProfileStyles';
 
 export default function ViewProfile() {
 
-    const styles = {
-        container: {
-            minWidth: "100vw",
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            flexDirection: 'column',
-            height: '80vh',
-        },
-        card: {
-            borderRadius: '10px',
-            minWidth: '500px',
-            maxWidth: '600px',
-            backgroundImage: 'url(\"https://www.example.com/background-image.jpg\")',
-            boxShadow: "0px 0px 3px 3px rgba(0, 0, 0, 0.2)",
-            '&:hover': {
-                cursor: 'pointer',
-                transform: 'scale(1.05)',
-                transition: 'transform 0.2s ease-in-out',
-            },
-            '&:not(:hover)': {
-                transform: 'scale(1)',
-                transition: 'transform 0.2s ease-in-out',
-              },
-        },
-        avatar: {
-            boxShadow: "0px 0px 3px 3px rgba(0, 0, 0, 0.2)"
-        },
-        header: {
-            backgroundColor: "#01CAB6",
-        },
-        data: {
-            marginTop: "10px",
-            marginBottom: "10px"
-        },
-        titleBox: { display: 'flex', alignItems: 'center', justifyContent: 'space-between' },
-        
-    }
-
+    const theme = useTheme();
+    
     const { viewUser } = useSelector((state) => state.view)
     const { name, lastName, email, profilePhoto, birthdayDate, nationality, phone, studies, gender, profession } = viewUser;
 
@@ -59,24 +26,41 @@ export default function ViewProfile() {
     }, [])
 
     return (
-        <Container sx={styles.container}>
-            <Card sx={styles.card}>
-                <CardHeader
-                    avatar={<Avatar sx={styles.avatar} src={profilePhoto} />}
-                    title={<Box sx={styles.titleBox}><Typography>{`${name} ${lastName}`}</Typography><RadioButtonUncheckedIcon/></Box>}
-                    sx={styles.header}
-                />
-                
-                <CardContent>
-                    <Typography variant='h5' sx={styles.data}>Correo electrónico: {email}</Typography>
-                    <Typography variant='h5' sx={styles.data}>Teléfono: {phone}</Typography>
-                    <Typography variant='h5' sx={styles.data}>Nacionalidad: {nationality}</Typography>
-                    <Typography variant='h5' sx={styles.data}>Profesión: {profession}</Typography>
-                    <Typography variant='h5' sx={styles.data}>Estudios: {studies}</Typography>
-                    <Typography variant='h5' sx={styles.data}>Fecha de nacimiento: {birthdayDate}</Typography>
-                    <Typography variant='h5' sx={styles.data}>Género: {gender}</Typography>
-                </CardContent>
-            </Card>
-        </Container>
+        <Grid container sx={{ ...styles.container, [theme.breakpoints.down('sm')]: { paddingTop: '20px' } }} maxWidth="false">
+            <Grid item sx={styles.header} sm={10} md={5}>
+                <Avatar sx={styles.header.avatar} src={profilePhoto} />
+                <Typography variant="h5">{name} {lastName}</Typography>
+            </Grid>
+            <Grid item sx={styles.body} sm={10} md={5}>
+                <Box sx={{ ...styles.body.info, borderTop: "none" }} id="primero">
+                    <Typography sx={styles.body.info.label}>Correo electrónico</Typography>
+                    <Typography sx={styles.body.info.data}>{email}</Typography>
+                </Box>
+                <Box sx={styles.body.info}>
+                    <Typography sx={styles.body.info.label}>Teléfono</Typography>
+                    <Typography sx={styles.body.info.data}>{phone}</Typography>
+                </Box>
+                <Box sx={styles.body.info}>
+                    <Typography sx={styles.body.info.label}>Nacionalidad</Typography>
+                    <Typography sx={styles.body.info.data}>{nationality}</Typography>
+                </Box>
+                <Box sx={styles.body.info}>
+                    <Typography sx={styles.body.info.label}>Profesión</Typography>
+                    <Typography sx={styles.body.info.data}>{profession}</Typography>
+                </Box>
+                <Box sx={styles.body.info}>
+                    <Typography sx={styles.body.info.label}>Estudios</Typography>
+                    <Typography sx={styles.body.info.data}>{studies}</Typography>
+                </Box>
+                <Box sx={styles.body.info}>
+                    <Typography sx={styles.body.info.label}>Fecha de nacimiento</Typography>
+                    <Typography sx={styles.body.info.data}>{birthdayDate}</Typography>
+                </Box>
+                <Box sx={styles.body.info}>
+                    <Typography sx={styles.body.info.label}>Género</Typography>
+                    <Typography sx={styles.body.info.data}>{gender}</Typography>
+                </Box>
+            </Grid>
+        </Grid>
     )
 }
