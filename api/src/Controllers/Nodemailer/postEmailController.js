754bc69814e1e66a2812398ce778dc2c17eb63ba
carlavega13 <109261938+carlavega13@users.noreply.github.com>
2023-05-08 15:18:23 -0300

    los mails ahora se mandan por html

--- api/src/Controllers/Nodemailer/postEmailController.js
+++ api/src/Controllers/Nodemailer/postEmailController.js
@@ -2,7 +2,6 @@ const sendEmail = require("./sendEmail");
 
 const postEmailController = async (req, res) => {
   try {
-    console.log("body", req.body);
     await sendEmail(req.body);
     res.status(200).json("Tu correo fue enviado");
   } catch (error) {
