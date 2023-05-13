ac22acdf1660afa318d673e9dd579b38d0f38f09
Hernan Rotellini <hernanrotellini@hotmail.com>
2023-05-13 16:45:29 -0300

    fix ruta post

--- api/src/Controllers/Supervisor/postSupervisor.js
+++ api/src/Controllers/Supervisor/postSupervisor.js
@@ -1,42 +1,46 @@
 const { Supervisor } = require("../../db");
 const bcrypt = require("bcrypt");
-
+const passwordGenerator = require("../passwordGenerator");
+const axios = require("axios")
 //Controlador para crear un Supervisor
 const postSupervisor = async (req, res) => {
   try {
-    const { email, password, rol } = req.body;
+    const { email, rol } = req.body;
     const exist = await Supervisor.findOne({ where: { email: email } });
-    if (exist !== null) {
+    if (exist) {
       return res.status(400).send("El supervisor ya existe");
     }
-    if (email === "admin@admin.com") {
-      const passwordHash = await bcrypt.hashSync(password, 10);
-      const startingSuperAdmin = await Supervisor.create({
-        email: email,
-        password: passwordHash,
-        rol: rol,
-      });
-      return res.status(200).json(startingSuperAdmin);
-    }
     //El superAdmin debe ingresar email y contraseña para poder crear un nuevo supervisor
-    if (email && password) {
-      // Generar hash de la contraseña
-      const passwordHash = await bcrypt.hashSync(password, 10);
-      //Crear el Supervisor con el email ingresado y password hasheada y si es superAdmin o no
-      if (rol === "SuperAdmin") {
+    if (email) {
+      const {password}= req.body
+     
+      if (password) {
+        // Generar hash de la contraseña
+        const passwordHash = bcrypt.hashSync(password, 10);
+        //Crear el Supervisor con el email ingresado y password hasheada y si es superAdmin o no
         const newSupervisor = await Supervisor.create({
           email: email,
           password: passwordHash,
           rol: rol,
         });
+        const user = {
+          email:newSupervisor.email,password:password,rol:newSupervisor.rol
+        }
+        axios.post("http://localhost:3001/postCreatedAccount", user);
         return res.status(201).json(newSupervisor);
       } else {
+        let pass = passwordGenerator(8)
+        const passwordHash = bcrypt.hashSync(pass, 10);
         const newSupervisor = await Supervisor.create({
           email: email,
           password: passwordHash,
           rol: rol,
         });
         //Retorna un objeto de tipo Supervisor con todos sus datos
+        const user = {
+          email:newSupervisor.email,password:pass,rol:newSupervisor.rol
+        }
+        axios.post("http://localhost:3001/postCreatedAccount", user);
         return res.status(201).json(newSupervisor);
       }
     } else {
