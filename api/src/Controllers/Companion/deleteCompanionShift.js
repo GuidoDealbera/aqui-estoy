05776624def0423e65deb35961395df350299428
Pedro Miranda Nowak <pedromirandanowak@gmail.com>
2023-05-16 14:08:21 -0300

    Timezones-Calendar

--- api/src/Controllers/Companion/deleteCompanionShift.js
+++ api/src/Controllers/Companion/deleteCompanionShift.js
@@ -1,16 +1,16 @@
-const { Companion , CompanionShift} = require("../../db");
+const { Companion, CompanionShift, CityTimeZone } = require("../../db");
 const deleteCompanionShift = async (req, res) => {
   try {
     const { id, idShift } = req.body;
-    let companion = await Companion.findOne({ where: { id: id } , include: { model: CompanionShift} });
-     await companion.removeCompanionShift(idShift);
-      companion.save();
-      companion = await Companion.findOne({ where: { id: id } , include: { model: CompanionShift} });
-    
-      return res.status(200).json(companion);   
- 
-    } catch (error) {
-   return res.status(400).json(error.message);
+    let companion = await Companion.findOne({ where: { id: id }, include: { model: CompanionShift } });
+    await companion.removeCompanionShift(idShift);
+    await companion.save();
+    companion = await Companion.findOne({ where: { id: id }, include: [{ model: CompanionShift }, { model: CityTimeZone }]});
+
+    return res.status(200).json(companion);
+
+  } catch (error) {
+    return res.status(400).json(error.message);
   }
 };
 module.exports = deleteCompanionShift;
